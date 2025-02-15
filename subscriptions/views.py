21d9faf54de0f680bbb1245ae5c9ffc6ab3f1a10
Chuqdi <morganhezekiah111@gmail.com>
2025-02-16 00:21:31 +0100

    g

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -259,7 +259,7 @@ class CreateSubscriptionIntent(APIView):
         
         
         
-        # price_1QXftWEaYyTuzzYVRuJV7WdV
+        # price_1NeZq5EaYyTuzzYVMmZ8MpWz
         # line_items =[
         #         {
         #            "price":priceId,
@@ -268,7 +268,7 @@ class CreateSubscriptionIntent(APIView):
         
         line_items =[
                 {
-                   "price":"price_1QXftWEaYyTuzzYVRuJV7WdV",
+                   "price":"price_1NeZq5EaYyTuzzYVMmZ8MpWz",
                     "quantity":1
         }]
         
