11801d42a2b5330fbd69a99a12e72fdff183ab0a
Chuqdi <morganhezekiah111@gmail.com>
2025-06-18 11:22:08 +0100

    g

--- subscriptions/stripe_webhook.py
+++ subscriptions/stripe_webhook.py
@@ -58,9 +58,8 @@ def stripe_webhook(request):
     # if event.type == 'customer.subscription.trial_will_end':
     #     handle_trial_will_end(event.data.object)
     # WHEN USER SUCCESSFUL SUBCRIBES
-    if event.type == "customer.subscription.created":
-        pass
-        # handle_subscription_created(event.data.object)
+    if event.type == "checkout.session.completed":
+        handle_subscription_created(event.data.object)
     # elif event.type == 'customer.subscription.updated':
         # handle_subscription_updated(event.data.object)
     # elif event.type == 'invoice.payment_succeeded':
